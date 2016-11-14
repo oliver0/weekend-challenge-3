ad483f21cd98bbf4100bc2a0abf7798a334f5775
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-14 13:11:41 -0600

    Add comments to client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,20 +7,24 @@ $(document).ready(function(){
   var negativeVisible = false;
   var decimalVisible = false;
 
+  // listener for clear button which calls clearView to reset all the variables
   $('#clear').on('click', function(){
     $('#view').text('0');
     clearView();
   });
 
 
-
+  // gives the number as a percentage when clicked
   $('#percent').on('click', function(){
     var percent = $('#view').text() / 100;
     $('#view').text(percent);
   });
 
+  // when clicked, number is either made negative, or, if already negative
+  // is made positive
   $('#negative').on('click', makeNegativeOrPositive);
 
+  // when clicked a decimal point is added to x or y and is shown on DOM
   $('#decimal').on('click', function(){
     if(!decimalVisible){
       if(operationClicked){
@@ -33,6 +37,8 @@ $(document).ready(function(){
     }
   });
 
+  // when a number is clicked it is added to either x or y and is also added
+  // to the DOM
   $('.number').on('click', function(){
     if(!operationClicked){
       x += $(this).text();
@@ -43,6 +49,8 @@ $(document).ready(function(){
     }
   });
 
+  // if a math operation is clicked the first number is now know and saved in
+  // in the numberObject. The type of operation is also added to the numberObject.
   $('.operations').on('click', function(){
     if (!operationClicked) {
       numberObject.x = x;
@@ -51,6 +59,8 @@ $(document).ready(function(){
     }
   });
 
+  // when equals is clicked the second number is added to numberObject. The url
+  // is determined by the type of operation and the data is the numberObject.
   $('#equals').on('click', function(){
     numberObject.y = y;
     var fileName = '/' + numberObject.type;
@@ -58,6 +68,8 @@ $(document).ready(function(){
       type: 'POST',
       url: fileName,
       data: numberObject,
+      // if the post is successful then another ajax call gets the result and
+      // everything is reset
       success: function(data){
         getResult();
         clearView();
@@ -65,6 +77,8 @@ $(document).ready(function(){
     });
   });
 
+  // this makes the number on the view either negative or positive and updates
+  // the x and y variables
   function makeNegativeOrPositive(){
     var currentNumber = $('#view').text();
     if(negativeVisible){
@@ -88,6 +102,7 @@ $(document).ready(function(){
     negativeVisible = !negativeVisible;
   }
 
+  // this function resets everything
   function clearView(){
     x = '';
     y = '';
@@ -95,6 +110,8 @@ $(document).ready(function(){
     operationClicked = false;
   }
 
+  // this function makes a get request to the url based on the type of mathematical
+  // operation. If it is successful it updates the view with the result
   function getResult(){
     var fileName = '/' + numberObject.type;
     $.ajax({
